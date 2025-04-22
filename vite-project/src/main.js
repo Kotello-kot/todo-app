03a0ce69553b476ca06c481e2b9a3f037f34db5e
kotello-kot <kotovadasha1102@gmail.com>
2025-04-22 13:11:47 +0200

    first commit

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,19 +1,27 @@
-const input = document.getElementById("taskInput");
-const addBtn = document.getElementById("addBtn");
-const taskList = document.getElementById("taskList");
+const input = document.getElementById('taskInput');
+const button = document.getElementById('addBtn');
+const list = document.getElementById('taskList');
 
-addBtn.addEventListener("click", () => {
-  const taskText = input.value.trim();
+button.addEventListener('click', () => {
+  const task = input.value.trim();
+  if (task === '') return;
 
-  if (taskText !== "") {
-    const li = document.createElement("li");
-    li.textContent = taskText;
+  const li = document.createElement('li');
+  li.textContent = task;
 
-    li.addEventListener("click", () => {
-      li.classList.toggle("done");
-    });
+  li.addEventListener('click', () => {
+    li.style.textDecoration = 'line-through';
+  });
 
-    taskList.appendChild(li);
-    input.value = "";
-  }
+  list.appendChild(li);
+  input.value = '';
 });
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: './vite-project', // Путь к папке с твоими исходными файлами
+  build: {
+    outDir: 'dist', // Папка, в которую будут собираться файлы при сборке
+  }
+})
